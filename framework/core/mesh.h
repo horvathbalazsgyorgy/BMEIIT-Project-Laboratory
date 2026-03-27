065d0b3dadcd1742a1d940d493a7e55b077fc72d
Horváth Balázs György <horvath.balazs.gyorgy@gmail.com>
2026-03-27 02:57:46 +0100

    Removed useless 'EmptyMesh' instantiation of Mesh

--- framework/core/mesh.h
+++ framework/core/mesh.h
@@ -9,7 +9,6 @@ namespace Framework {
         Material* boundMaterial;
         void init() { createMesh(); }
     public:
-        Mesh() = default;
         Mesh(Material* boundMaterial) {
             if (!boundMaterial)
                 throw std::invalid_argument("Invalid argument; expected material but found none.");
@@ -22,17 +21,6 @@ namespace Framework {
             boundMaterial->draw();
         }
         virtual ~Mesh() = default;
-    };
-
-    //NOTE: For test purposes
-    class EmptyMesh : public Mesh {
-    public:
-        void createMesh() override {
-            /*nop*/
-        }
-        void draw() override {
-            /*nop*/
-        }
     };
 }
 
