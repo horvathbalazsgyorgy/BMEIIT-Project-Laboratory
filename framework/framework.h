f5260b1dcb098fb1a659c2718453bcf53ed2ef4a
Horváth Balázs György <horvath.balazs.gyorgy@gmail.com>
2026-03-28 01:14:29 +0100

    Reorganized and adjusted framework.h file to include entire framework

--- framework/framework.h
+++ framework/framework.h
@@ -1,20 +1,30 @@
 #ifndef PROJECTLABORATORY_FRAMEWORK_H
 #define PROJECTLABORATORY_FRAMEWORK_H
 
-#include "opengl/application.h"
 #include "core/camera.h"
 #include "core/light.h"
 #include "core/lightarray.h"
 #include "core/material.h"
 #include "core/mesh.h"
 #include "core/model.h"
-#include "utility/mouse.h"
+
+#include "opengl/loader/shaderloader.h"
+#include "opengl/loader/texture.h"
+
+#include "opengl/render/framebuffer.h"
+#include "opengl/render/rendertarget.h"
+#include "opengl/render/rendertexture.h"
+
+#include "opengl/application.h"
+#include "opengl/shaderprogram.h"
+
 #include "scene/scene.h"
 #include "scene/scenebuilder.h"
-#include "opengl/loader/shaderloader.h"
-#include "opengl/shaderprogram.h"
-#include "opengl/loader/texture.h"
+
 #include "uniform/uniform.h"
 #include "uniform/uniformregistry.h"
+#include "uniform/uniformsource.h"
+
+#include "utility/mouse.h"
 
 #endif //PROJECTLABORATORY_FRAMEWORK_H
