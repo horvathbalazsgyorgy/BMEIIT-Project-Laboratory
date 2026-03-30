76ad87e54ddd8e9187450ba62b52b7ecbb9763b6
Horváth Balázs György <horvath.balazs.gyorgy@gmail.com>
2026-03-31 00:26:40 +0200

    Added normalMatrix calculation and corresponding auto uniform support to Model class

--- framework/core/model.cpp
+++ framework/core/model.cpp
@@ -8,8 +8,9 @@
 
 namespace Framework {
     void Model::initDump() {
-        dump.variables[glslPrefix + ".modelMatrix"] = &modelMatrix;
-        dump.variables[glslPrefix + ".position"] = &position;
+        dump.variables[glslPrefix + ".modelMatrix"]  = &modelMatrix;
+        dump.variables[glslPrefix + ".normalMatrix"] = &normalMatrix;
+        dump.variables[glslPrefix + ".position"]     = &position;
     }
 
     Model::Model(ShaderProgram* program, glm::vec3 position, glm::vec3 scale, const std::string& prefix)
@@ -23,9 +24,10 @@ namespace Framework {
     }
 
     void Model::update() {
-        modelMatrix = glm::mat4(1.0f);
-        modelMatrix = glm::scale(modelMatrix, scale);
-        modelMatrix = glm::translate(modelMatrix, position);
+        modelMatrix  = glm::mat4(1.0f);
+        modelMatrix  = glm::translate(modelMatrix, position);
+        modelMatrix  = glm::scale(modelMatrix, scale);
+        normalMatrix = glm::transpose(glm::inverse(glm::mat3(modelMatrix)));
     }
     
     void Model::draw() {
--- framework/core/model.h
+++ framework/core/model.h
@@ -13,9 +13,10 @@ namespace Framework {
     class Material;
 
     class Model : public UniformSource {
-        glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f);
-        glm::vec3 scale = glm::vec3(1.0f, 1.0f, 1.0f);
-        glm::mat4 modelMatrix = glm::mat4(1.0f);
+        glm::vec3 position     = glm::vec3(0.0f, 0.0f, 0.0f);
+        glm::vec3 scale        = glm::vec3(1.0f, 1.0f, 1.0f);
+        glm::mat4 modelMatrix  = glm::mat4(1.0f);
+        glm::mat3 normalMatrix = glm::mat3(1.0f);
 
         void initDump() override;
     protected:
