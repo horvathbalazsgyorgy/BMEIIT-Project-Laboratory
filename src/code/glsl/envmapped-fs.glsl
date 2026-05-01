228ef9e5bf3dd44c76a72a9025c16e476d855493
Horváth Balázs György <horvath.balazs.gyorgy@gmail.com>
2026-05-01 18:44:56 +0200

    Removed tone mapping from envmapped-fs.glsl shader file

--- src/code/glsl/envmapped-fs.glsl
+++ src/code/glsl/envmapped-fs.glsl
@@ -8,13 +8,9 @@ uniform struct{
     samplerCube envTexture;
 } material;
 
-uniform float exposure;
-
 out vec4 fragmentColor;
 
 void main(void) {
     vec3 color = texture(material.envTexture, rayDir.xyz).rgb;
-    vec3 toneMapped = vec3(1.0f) - exp(-color * exposure);
-
-    fragmentColor = vec4(toneMapped, 1.0f);
+    fragmentColor = vec4(color, 1.0f);
 }
