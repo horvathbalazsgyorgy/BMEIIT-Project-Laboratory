d4010d66d7f1a13d66a2337a4ef2e2d3fa337842
Horváth Balázs György <horvath.balazs.gyorgy@gmail.com>
2026-05-01 16:01:34 +0200

    Slight modifications in main.cpp and CMake

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include "framework/framework.h"
-#include "test/builder3d.h"
+#include "code/scene/builder/builder3d.h"
 
 using namespace Framework;
 
@@ -7,7 +7,7 @@ int main() {
     GLApplication::initializeGL(4, 3);
     GLApplication::createWindow(1280, 720);
 
-    SceneBuilder* builder = new Builder3D();
+    auto* builder = new Builder3D();
     auto* scene = new Scene(builder);
     scene->buildScene();
 
