ca8cb04c7a4378eef383fc8f6b255f62da81e938
Horváth Balázs György <horvath.balazs.gyorgy@gmail.com>
2026-05-01 18:46:24 +0200

    Added multi-render target support to equirectangular shader

--- src/code/glsl/equirectangular-fs.glsl
+++ src/code/glsl/equirectangular-fs.glsl
@@ -5,11 +5,14 @@ precision highp float;
 in vec4 rayDir;
 
 uniform struct{
-    sampler2D equirectangularMap;
+    sampler2D panoramaLDR;
+    sampler2D panoramaHDR;
 } material;
 
-out vec4 fragmentColor;
-const vec2 invAtan = vec2(0.1591f, 0.3183f);
+layout (location = 0) out vec4 ldrColor;
+layout (location = 1) out vec4 hdrColor;
+const float PI = 3.14159265359;
+const vec2 invAtan = vec2(1.0/(2.0 * PI), 1.0/PI);
 
 vec2 sphericalToCartesian(vec3 direction){
     vec2 uv = vec2(atan(direction.z, direction.x), asin(direction.y));
@@ -20,7 +23,9 @@ vec2 sphericalToCartesian(vec3 direction){
 
 void main(void) {
     vec2 uv = sphericalToCartesian(normalize(rayDir.xyz));
-    vec3 color = texture(material.equirectangularMap, uv).rgb;
+    vec3 ldr = texture(material.panoramaLDR, uv).rgb;
+    vec3 hdr = texture(material.panoramaHDR, uv).rgb;
 
-    fragmentColor = vec4(color, 1.0f);
+    ldrColor = vec4(ldr, 1.0f);
+    hdrColor = vec4(hdr, 1.0f);
 }
