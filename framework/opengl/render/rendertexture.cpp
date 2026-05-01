a338d6363d4f538d557a7d26ed58a3b1cd3e8d40
Horváth Balázs György <horvath.balazs.gyorgy@gmail.com>
2026-05-01 12:29:48 +0200

    Restructured RenderTexture inheritance hierarchy and added customization options/support for cubemaps

--- framework/opengl/render/rendertexture.cpp
+++ framework/opengl/render/rendertexture.cpp
@@ -1,22 +1,76 @@
 #include "rendertexture.h"
 
 namespace Framework {
+    /**RenderTexture2D**/
     RenderTexture2D::RenderTexture2D(
         const int width,
         const int height,
-        const GLenum internalFormat,
-        const GLenum format,
-        const GLenum type)
+        const TextureProperties &properties) : RenderTexture(properties)
     {
+        auto mipmap  = properties.filter == TRILINEAR;
+        auto filtering = properties.filter == TRILINEAR ? GL_LINEAR : properties.filter;
+
         glBindTexture(GL_TEXTURE_2D, texture);
-        glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, format, type, nullptr);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexImage2D(
+            GL_TEXTURE_2D, 0,
+            properties.internalFormat, width, height, 0,
+            properties.format, properties.type, nullptr);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, properties.filter);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, filtering);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, properties.wrap);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, properties.wrap);
+        if (mipmap)
+            glGenerateMipmap(GL_TEXTURE_2D);
+        glBindTexture(GL_TEXTURE_2D, 0);
     }
 
-    void RenderTexture2D::bindBufferTexture(const GLenum attachment) const {
-        glFramebufferTexture2D(GL_FRAMEBUFFER, attachment, GL_TEXTURE_2D, texture, 0);
+    void RenderTexture2D::bindBufferTexture(const GLenum attachment, const int mipLevel) const {
+        glFramebufferTexture2D(GL_FRAMEBUFFER, attachment, GL_TEXTURE_2D, texture, mipLevel);
+    }
+
+    void RenderTexture2D::makeMipmap() {
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, TRILINEAR);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        glBindTexture(GL_TEXTURE_2D, 0);
+    }
+
+    /**RenderTextureCube**/
+    RenderTextureCube::RenderTextureCube(
+        const int width,
+        const int height,
+        const TextureProperties& properties) : RenderTexture(properties)
+    {
+        auto mipmap  = properties.filter == TRILINEAR;
+        auto filtering = properties.filter == TRILINEAR ? GL_LINEAR : properties.filter;
+
+        glBindTexture(GL_TEXTURE_CUBE_MAP, texture);
+        for (int i = 0; i < 6; i++) {
+            glTexImage2D(
+                GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0,
+                properties.internalFormat, width, height, 0,
+                properties.format, properties.type, nullptr);
+        }
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, properties.filter);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, filtering);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, properties.wrap);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, properties.wrap);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, properties.wrap);
+        if (mipmap)
+            glGenerateMipmap(GL_TEXTURE_CUBE_MAP);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+    }
+
+    void RenderTextureCube::bindBufferTexture(GLenum attachment, int mipLevel) const {
+        auto face = mipLevel % 6;
+        auto mip  = mipLevel / 6;
+        glFramebufferTexture2D(GL_FRAMEBUFFER, attachment, GL_TEXTURE_CUBE_MAP_POSITIVE_X + face, texture, mip);
+    }
+
+    void RenderTextureCube::makeMipmap() {
+        glBindTexture(GL_TEXTURE_CUBE_MAP, texture);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, TRILINEAR);
+        glGenerateMipmap(GL_TEXTURE_CUBE_MAP);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
     }
 }
--- framework/opengl/render/rendertexture.h
+++ framework/opengl/render/rendertexture.h
@@ -5,17 +5,39 @@
 #include "glad/glad.h"
 
 namespace Framework {
-    class RenderTexture2D : public Texture2D {
-    public:
-        RenderTexture2D(
-            int width,
-            int height,
-            GLenum internalFormat,
-            GLenum format,
-            GLenum type
-        );
+    enum TextureFiltering {
+        POINT = GL_NEAREST,
+        BILINEAR = GL_LINEAR,
+        TRILINEAR = GL_LINEAR_MIPMAP_LINEAR,
+    };
 
-        void bindBufferTexture(GLenum attachment) const;
+    struct TextureProperties {
+        GLenum internalFormat, format, type;
+        GLint  wrap;
+        TextureFiltering filter;
+    };
+
+    class RenderTexture : virtual public Texture {
+    public:
+        TextureProperties properties;
+
+        RenderTexture(const TextureProperties &properties) : properties(properties) {}
+        virtual void bindBufferTexture(GLenum attachment, int mipLevel) const = 0;
+        virtual void makeMipmap() = 0;
+    };
+
+    class RenderTexture2D : public RenderTexture, public Texture2D {
+    public:
+        RenderTexture2D(int width, int height, const TextureProperties &properties);
+        void bindBufferTexture(GLenum attachment, int mipLevel) const override;
+        void makeMipmap() override;
+    };
+
+    class RenderTextureCube : public RenderTexture, public TextureCube {
+    public:
+        RenderTextureCube(int width, int height, const TextureProperties &properties);
+        void bindBufferTexture(GLenum attachment, int mipLevel) const override;
+        void makeMipmap() override;
     };
 }
 
