f794c1d3f6edeebdbb7ce74025e81cbb8df28ca4
Horváth Balázs György <horvath.balazs.gyorgy@gmail.com>
2026-05-04 02:44:45 +0200

    Removed tone-mapping from post-process pass and moved it to their respective shaders

--- src/code/glsl/envmapped-fs.glsl
+++ src/code/glsl/envmapped-fs.glsl
@@ -5,12 +5,14 @@ precision highp float;
 in vec4 rayDir;
 
 uniform struct{
+    float exposure;
     samplerCube envTexture;
 } material;
 
 out vec4 fragmentColor;
 
 void main(void) {
-    vec3 color = texture(material.envTexture, rayDir.xyz).rgb;
-    fragmentColor = vec4(color, 1.0f);
+    vec3 color = textureLod(material.envTexture, rayDir.xyz, 0.5).rgb;
+    vec3 toneMapped = vec3(1.0f) - exp(-color * material.exposure);
+    fragmentColor = vec4(toneMapped, 1.0f);
 }
