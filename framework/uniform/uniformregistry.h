8598bf6311a2b3b063e396a5af061990751eaf25
Horváth Balázs György <horvath.balazs.gyorgy@gmail.com>
2026-05-04 02:08:28 +0200

    Slight modifications regarding includes in UniformRegistry

--- framework/uniform/uniformregistry.h
+++ framework/uniform/uniformregistry.h
@@ -4,11 +4,10 @@
 #include <string>
 #include <memory>
 #include <unordered_map>
+#include "uniform.h"
 #include "glad/glad.h"
 
 namespace Framework {
-    class Uniform;
-
     class UniformRegistry {
         std::unordered_map<std::string, std::unique_ptr<Uniform>> uniforms;
     public:
