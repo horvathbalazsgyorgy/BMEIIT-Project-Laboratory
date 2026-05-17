22a52910656d3b71d385e8612475390fe34613de
Horváth Balázs György <horvath.balazs.gyorgy@gmail.com>
2026-05-18 01:30:22 +0200

    Class Texture2D is now capable of asynchronous loading in the background

--- framework/opengl/loader/texture.h
+++ framework/opengl/loader/texture.h
@@ -2,32 +2,50 @@
 #define PROJECTLABORATORY_TEXTURE_H
 
 #include <string>
+#include "framework/threading/job.h"
 #include "glad/glad.h"
 
 namespace Framework {
     enum TextureEncoding {
-        GRAYSCALE = GL_RED,
+        GRAYSCALE = GL_R8,
         LINEAR = GL_RGB8,
         sRGB = GL_SRGB8
+    };
+
+    struct TextureJobState {
+        std::string path;
+        TextureEncoding encoding;
+        GLenum format;
+        int width, height, channels;
+        unsigned int iPixel;
+        unsigned char* pixels;
+        bool initialized;
     };
 
     class Texture {
     protected:
         GLuint texture;
     public:
-        Texture() {
-            glGenTextures(1, &texture);
-        }
+        Texture() { glGenTextures(1, &texture); }
+        Texture(const unsigned int ID) { texture = ID; }
+        [[nodiscard]] GLuint ID() const { return texture; }
         virtual void bindTexture(int samplerIndex) const = 0;
         virtual ~Texture() = default;
     };
 
 
-    class Texture2D : virtual public Texture {
+    class Texture2D : virtual public Texture, public JobVisitor {
+        TextureJobState state;
+        void createTexture();
+        void streamTexture();
+        static constexpr size_t chunk = 32768;
     public:
         Texture2D() = default;
         Texture2D(TextureEncoding encoding, const std::string &filePath);
+        Texture2D(unsigned int ID, TextureEncoding encoding, const std::string &filePath);
         void bindTexture(int samplerIndex) const override;
+        void run() override;
+        bool complete() override;
     };
 
 
