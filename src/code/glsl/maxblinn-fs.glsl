ced39e2349de50834271559e9b0d34cb7f5bf60d
Horváth Balázs György <horvath.balazs.gyorgy@gmail.com>
2026-05-31 11:01:46 +0200

    Reworked the Max-Blinn shader to work smoothly together with the new G-buffer

--- src/code/glsl/maxblinn-fs.glsl
+++ src/code/glsl/maxblinn-fs.glsl
@@ -2,31 +2,37 @@
 
 precision highp float;
 
+in vec4 viewPosition;
 in vec2 tex;
 
 uniform struct{
-    sampler2D gPosition;
     sampler2D gNormal;
     sampler2D gAlbedo;
 } material;
 
-uniform struct{
+uniform struct {
+    mat4 view;
+    mat4 projection;
+    mat4 invView;
+    mat4 invProjection;
     vec3 position;
 } camera;
 
 uniform struct{
     vec4 position;
     vec3 emittance;
-    vec3 ambient;
-} lights[4];
+} lights[6];
 
-out vec4 fragmentColor;
+uniform int ambient;
+uniform sampler2D SSAO;
 
-vec4 shade(vec3 normal, vec3 lightDir, vec3 viewDir, vec3 albedo, vec3 emittance, vec3 ambient, float distance){
+layout (location = 0) out vec3 fragmentColor;
+
+vec3 shade(vec3 normal, vec3 lightDir, vec3 viewDir, vec3 albedo, vec3 emittance, float ambient, float distance){
     vec3 kd = albedo;
     vec3 ks = vec3(1.0f, 1.0f, 1.0f);
 
-    vec3 ambientColor = ambient * kd;
+    vec3 ambientColor = 0.05 * ambient * kd;
 
     float cosa = max(dot(normal, lightDir), 0.0f);
     vec3 diffuseColor = emittance * kd * cosa;
@@ -41,25 +47,37 @@ vec4 shade(vec3 normal, vec3 lightDir, vec3 viewDir, vec3 albedo, vec3 emittance
     diffuseColor  *= attenuation;
     specularColor *= attenuation;
 
-    return vec4(ambientColor + diffuseColor + specularColor, 1.0f);
+    return vec3(ambientColor + diffuseColor + specularColor);
 }
 
 void main(void) {
-    fragmentColor = vec4(0.0f, 0.0f, 0.0f, 1.0f);
+    fragmentColor = vec3(0.0f, 0.0f, 0.0f);
 
-    vec3 position  = texture(material.gPosition, tex).rgb;
-    vec3 normal    = texture(material.gNormal,   tex).rgb;
-    vec3 albedo    = texture(material.gAlbedo,   tex).rgb;
-    normal = normalize(normal);
+    //Extracting data from G-Buffer
+    vec4  Locations = texture(material.gNormal, tex);
 
-    vec3 viewDir   = normalize(camera.position - position);
+    vec3  viewRay  = viewPosition.xyz;
+    float depth    = Locations.w;
+    vec3  viewPos  = viewRay * depth;
+    vec4  worldPos = camera.invView * vec4(viewPos, 1.0f);
+    vec3  position = worldPos.xyz / worldPos.w;
 
-    for (int i = 0; i < 4; i++){
-        vec3 lightDiff = lights[i].position.xyz - lights[i].position.w * position;
+    vec3  normal    = normalize(Locations.xyz);
+    vec3  albedo    = texture(material.gAlbedo, tex).rgb;
+    float occlusion = texture(SSAO, tex).r;
+
+    vec3  viewDir  = normalize(camera.position - position);
+
+    if(ambient == 0){
+        occlusion = 1.0f;
+    }
+
+    for (int i = 0; i < 6; i++){
+        vec3 lightDiff = lights[i].position.xyz - lights[i].position.w * position.xyz;
         vec3 lightDir  = normalize(lightDiff);
         float distance = length(lightDiff);
 
-        fragmentColor += shade(normal, lightDir, viewDir, albedo, lights[i].emittance, lights[i].ambient, distance);
+        fragmentColor += shade(normal, lightDir, viewDir, albedo, lights[i].emittance, occlusion, distance);
     }
     fragmentColor = clamp(fragmentColor, 0.0, 1.0);
 }
