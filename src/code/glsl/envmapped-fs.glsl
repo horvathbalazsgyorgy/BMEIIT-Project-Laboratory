9a5eada1970a2e8e5ec87a3de5ad828b17027f42
Horváth Balázs György <horvath.balazs.gyorgy@gmail.com>
2026-05-31 10:53:04 +0200

    Removed tone-mapping from environment-fs.glsl

--- src/code/glsl/envmapped-fs.glsl
+++ src/code/glsl/envmapped-fs.glsl
@@ -5,14 +5,11 @@ precision highp float;
 in vec4 rayDir;
 
 uniform struct{
-    float exposure;
     samplerCube envTexture;
 } material;
 
-out vec4 fragmentColor;
+layout (location = 0) out vec3 fragmentColor;
 
 void main(void) {
-    vec3 color = textureLod(material.envTexture, rayDir.xyz, 0.5).rgb;
-    vec3 toneMapped = vec3(1.0f) - exp(-color * material.exposure);
-    fragmentColor = vec4(toneMapped, 1.0f);
+    fragmentColor = textureLod(material.envTexture, rayDir.xyz, 0.0).rgb;
 }
