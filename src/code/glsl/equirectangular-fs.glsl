6c976e728b9af6782f55e7cb834b305ab68d86a9
Horváth Balázs György <horvath.balazs.gyorgy@gmail.com>
2026-05-31 10:51:27 +0200

    Removed obsolete variables and render targets from equirectangular-fs.glsl

--- src/code/glsl/equirectangular-fs.glsl
+++ src/code/glsl/equirectangular-fs.glsl
@@ -6,11 +6,9 @@ in vec4 rayDir;
 
 uniform struct{
     sampler2D hdrTexture;
-    sampler2D envTexture;
 } material;
 
 layout (location = 0) out vec4 hdrColor;
-layout (location = 1) out vec4 envColor;
 const float PI = 3.14159265359;
 const vec2 invAtan = vec2(1.0/(2.0 * PI), 1.0/PI);
 
@@ -24,8 +22,5 @@ vec2 sphericalToCartesian(vec3 direction){
 void main(void) {
     vec2 uv = sphericalToCartesian(normalize(rayDir.xyz));
     vec3 hdr = texture(material.hdrTexture, uv).rgb;
-    vec3 ldr = texture(material.envTexture, uv).rgb;
-
     hdrColor = vec4(hdr, 1.0f);
-    envColor = vec4(ldr, 1.0f);
 }
